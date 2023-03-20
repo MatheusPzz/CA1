d021f5f3db0b4dd9072764d24246c287c9aae984
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-03-20 13:48:56 +0000

    Update README.md for CA1

--- app/src/main/java/com/example/mobileapps1/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mobileapps1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val button4 = findViewById<Button>(R.id.button4)
+        button4.setOnClickListener{
+            finish()
+        }
+
+
     }
 }
